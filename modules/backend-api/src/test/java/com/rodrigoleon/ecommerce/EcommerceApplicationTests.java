4d6ba9ce4f63dba9cba03cf5ec7d548fda55dd9c
Rodrigo Leon <rodrigoleonmdp@gmail.com>
2022-06-16 21:51:36 -0300

    Initial structure: Backend + Frontend

---
+++ modules/backend-api/src/test/java/com/rodrigoleon/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigoleon.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
